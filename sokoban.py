0c42b44df48a28f02998e9b5a8e3f2f14cc1746f
GuadalupeVM <97322599+GuadalupeVM@users.noreply.github.com>
2022-04-06 10:38:18 -0500

    Mapa
    El mapa  del primer nivel se carga desde un archivo de texto

--- sokoban.py
+++ sokoban.py
@@ -1,66 +1,70 @@
 from os import system, name
+    
+
 class sokoban1:
+#0 - Muñeco
+    #1 - Espacio
+    #2 - Caja
+    #3 - Paredes
+    #4 - Metas
+
+    # Controles
+    # a - Izquirda
+    # d - Derecha
+    # w - Arriba
+    # s - Abajo
 
     def __init__(self): # Se coloca un constructor 
         pass    
-        
 
     muneco_fila =  6   #encuentra la posicion del muñeco
     muneco_columna = 4
 
-    mapa = [
-        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-        [3, 3, 3, 3, 3, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],   #Diseño del mapa del juego 
-        [3, 3, 3, 3, 3, 2, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-        [3, 3, 3, 3, 3, 1, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-        [3, 3, 3, 1, 1, 2, 0, 2, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-        [3, 3, 3, 1, 3, 1, 3, 3, 1, 3, 3 ,3 ,3, 3, 3 ,3, 3, 3, 3],
-        [3, 1, 1, 1, 3, 1, 3, 3, 1, 3, 3, 3, 3, 3, 1, 1, 4 ,4, 3],
-        [3, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 3],
-        [3, 3, 3, 3, 3, 1, 3, 3, 3, 1, 3, 1, 3, 3, 1 ,1 ,4 ,4, 3],
-        [3, 3, 3 ,3 ,3, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
-    ]
-    
+    archivo = open('mapa1.txt', mode='r')
+    texto = archivo.read()
+    archivo.close()
+
+
+
     def clear(self):
         if name == 'nt': #win.
             system ('cls')
         else: 
             system ('clear')
- 
+
     def imprimirmapa (self):       #imprime mapa 
-        for fila in self.mapa:
-            for columna in fila:   #recorre cada elemnto de cada fila
-                if columna==3:
-                    print(chr(129521), end=" ")  #Cambia de valor al 3 (pared)
-                elif columna==1:
-                    print(" ", end="  ") #imprime un espacio vacio
-                elif columna==0:
-                    print(chr(128522), end=" ") #cambia de valor al persoaje y lo imprime 
-                elif columna==2:
-                    print(chr(128230), end= " ")#Cabia de valor a la caja 2  y la imprime
-                elif columna==4:
-                    print(chr(128681), end=" ")#Cambia de valor a la meta y la imprime 
-                elif columna==5:
-                    print(chr(128515), end=" ")#Cambia d evalor a personaje_meta 
-                elif columna==6:
-                    print(chr(127873), end=" ")#Cambia de valor a caja_meta
-                else:
-                    print(columna, end = " ")
-            print() 
+        for fila in self.texto:     #recorre las filas del mapa 
+                                    #recorre cada elemnto de cada fila
+            if fila=='3':
+                print(chr(129521), end=" ")  #Cambia de valor al 3 (pared)
+            elif fila=='1':
+                print("  ", end=" ") #imprime un espacio vacio
+            elif fila=='0':
+                print(chr(128522), end=" ") #cambia de valor al persoaje y lo imprime 
+            elif fila=='2':
+                print(chr(128230), end= " ")#Cabia de valor a la caja 2  y la imprime
+            elif fila=='4':
+                print(chr(128681), end=" ")#Cambia de valor a la meta y la imprime 
+            elif fila=='5':
+                print(chr(128515), end=" ")#Cambia d evalor a personaje_meta 
+            elif fila=='6':
+                print(chr(127873), end=" ")#Cambia de valor a caja_meta
+            else:
+                print(fila, end = "")
             
-    def moverDerecha(self):     #Funcion para los movimientos de Derecha
-        if self.mapa[self.muneco_columna][self.muneco_fila]==0 and self.mapa[self.muneco_columna][self.muneco_fila+1]==1: #Personaje, epacio
+
+    def moverDerecha(self):
+        if self.mapa[self.muneco_columna][self.muneco_fila]==0 and self.mapa[self.muneco_columna][self.muneco_fila+1]==1:
             self.mapa[self.muneco_columna][self.muneco_fila]=1
             self.mapa[self.muneco_columna][self.muneco_fila+1]=0
             self.muneco_fila+=1
-        elif self.mapa[self.muneco_columna][self.muneco_fila]==0 and self.mapa[self.muneco_columna][self.muneco_fila+1]==4:  #Personaje, meta
+        elif self.mapa[self.muneco_columna][self.muneco_fila]==0 and self.mapa[self.muneco_columna][self.muneco_fila+1]==4: 
             self.mapa[self.muneco_columna][self.muneco_fila]=1
             self.mapa[self.muneco_columna][self.muneco_fila+1]=5
             self.muneco_fila+=1
         elif self.mapa[self.muneco_columna][self.muneco_fila]==0 and self.mapa[self.muneco_columna][self.muneco_fila+1]==2 and self.mapa[self.muneco_columna][self.muneco_fila+2]==1: 
             self.mapa[self.muneco_columna][self.muneco_fila]=1
-            self.mapa[self.muneco_columna][self.muneco_fila+1]=0  #personaje, caja, meta
+            self.mapa[self.muneco_columna][self.muneco_fila+1]=0
             self.mapa[self.muneco_columna][self.muneco_fila+2]=2
             self.muneco_fila+=1
         elif self.mapa[self.muneco_columna][self.muneco_fila]==0 and self.mapa[self.muneco_columna][self.muneco_fila+1]==2 and self.mapa[self.muneco_columna][self.muneco_fila+2]==4: 
@@ -72,7 +76,7 @@ class sokoban1:
             self.mapa[self.muneco_columna][self.muneco_fila]=1
             self.mapa[self.muneco_columna][self.muneco_fila+1]=5
             self.mapa[self.muneco_columna][self.muneco_fila+2]=2
-            self.muneco_fila+=1 
+            self.muneco_fila+=1
         elif self.mapa[self.muneco_columna][self.muneco_fila]==0 and self.mapa[self.muneco_columna][self.muneco_fila+1]==6 and self.mapa[self.muneco_columna][self.muneco_fila+2]==4: 
             self.mapa[self.muneco_columna][self.muneco_fila]=1
             self.mapa[self.muneco_columna][self.muneco_fila+1]=5
@@ -106,8 +110,8 @@ class sokoban1:
             self.mapa[self.muneco_columna][self.muneco_fila+1]=5
             self.mapa[self.muneco_columna][self.muneco_fila+2]=6
             self.muneco_fila+=1
-            
-    def moverIzquierda(self):      #Funcion para mover a izquierda 
+
+    def moverIzquierda(self):
         if self.mapa[self.muneco_columna][self.muneco_fila]==0 and self.mapa[self.muneco_columna][self.muneco_fila-1]==1:
             self.mapa[self.muneco_columna][self.muneco_fila]=1
             self.mapa[self.muneco_columna][self.muneco_fila-1]=0
@@ -163,7 +167,7 @@ class sokoban1:
             self.mapa[self.muneco_columna][self.muneco_fila]=4
             self.mapa[self.muneco_columna][self.muneco_fila-1]=5
             self.mapa[self.muneco_columna][self.muneco_fila-2]=6
-            
+
     def moverArriba(self):
         if self.mapa[self.muneco_columna][self.muneco_fila]==0 and self.mapa[self.muneco_columna-1][self.muneco_fila]==1:
             self.mapa[self.muneco_columna][self.muneco_fila]=1
@@ -206,7 +210,7 @@ class sokoban1:
             self.mapa[self.muneco_columna-1][self.muneco_fila]=0
             self.mapa[self.muneco_columna-2][self.muneco_fila]=2
             self.muneco_columna-=1
-         elif self.mapa[self.muneco_columna][self.muneco_fila]==5 and self.mapa[self.muneco_columna-1][self.muneco_fila]==2 and self.mapa[self.muneco_columna-2][self.muneco_fila]==4: 
+        elif self.mapa[self.muneco_columna][self.muneco_fila]==5 and self.mapa[self.muneco_columna-1][self.muneco_fila]==2 and self.mapa[self.muneco_columna-2][self.muneco_fila]==4: 
             self.mapa[self.muneco_columna][self.muneco_fila]=4
             self.mapa[self.muneco_columna-1][self.muneco_fila]=0
             self.mapa[self.muneco_columna-2][self.muneco_fila]=6
@@ -221,7 +225,7 @@ class sokoban1:
             self.mapa[self.muneco_columna-1][self.muneco_fila]=5
             self.mapa[self.muneco_columna-2][self.muneco_fila]=6
             self.muneco_columna-=1
-            
+
     def moverAbajo(self):
         if self.mapa[self.muneco_columna][self.muneco_fila]==0 and self.mapa[self.muneco_columna+1][self.muneco_fila]==1:
             self.mapa[self.muneco_columna][self.muneco_fila]=1
@@ -280,15 +284,16 @@ class sokoban1:
             self.mapa[self.muneco_columna+2][self.muneco_fila]=6
             self.muneco_columna+=1
 
-juego=sokoban1()
-juego.imprimirmapa() 
 
+
+
+juego=sokoban1() #crea el objeto de la clase
 
 while True:              #crea el blucle hasta que se acabe el nivel o se slaga
     juego.clear()
     juego.imprimirmapa()
     
-    a=input() #ingresa el movimento #lee el mviento 
+    a=input() #ingresa el movimento
     if a=='d': #lee el moviemnto
         juego.moverDerecha()  #hace el mieviento
     elif a=='a':
@@ -299,3 +304,4 @@ while True:              #crea el blucle hasta que se acabe el nivel o se slaga
         juego.moverAbajo()
     if a=='q':
         break
+
